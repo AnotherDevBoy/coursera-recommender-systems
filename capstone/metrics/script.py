6169d1264f9ceb252a27c3076db6a4e8a5f919d9
coderlifter <ercalbwar@gmail.com>
2018-07-10 01:04:21 +0100

    Encapsulating MRR logic

--- capstone/metrics/script.py
+++ capstone/metrics/script.py
@@ -1,5 +1,25 @@
 import pandas as pd
 
+def mrr(ratings, top_n):
+    mrr = 0.0
+    for user_id in users:
+        user_ratings = ratings[user_id].dropna()
+
+        rank = 1.0
+
+        for recommendation in top_n.iterrows():
+            recommended_item =  recommendation[0]
+
+            if recommended_item in user_ratings:
+                mrr += 1/rank
+                print 'User', user_id, 'rated item', recommended_item, 'rank', rank
+            
+            rank += 1.0
+
+    number_of_users = 1.0 * len(users)
+
+    return  mrr/number_of_users
+
 headers = pd.read_csv('cbf.csv', nrows=1)
 
 users = headers.columns[1:] 
@@ -14,20 +34,4 @@ predictions_for_user = predictions[[user_id]]
 predictions_for_user = predictions_for_user.sort_values(by=[user_id], ascending=False)
 top_n = predictions_for_user.head()
 
-mrr = 0.0
-for user_id in users:
-    user_ratings = ratings[user_id].dropna()
-
-    rank = 1.0
-
-    for recommendation in top_n.iterrows():
-        recommended_item =  recommendation[0]
-
-        if recommended_item in user_ratings:
-            mrr += 1/rank
-            print 'User', user_id, 'rated item', recommended_item, 'rank', rank
-        
-        rank += 1.0
-
-number_of_users = 1.0 * len(users)
-print 'MRR', mrr/number_of_users
+print 'MRR', mrr(ratings, top_n)
