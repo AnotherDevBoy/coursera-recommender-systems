071a4c1a339fbf5b57e02423de9f4b9511a27b84
coderlifter <ercalbwar@gmail.com>
2018-07-10 21:16:54 +0100

    Some metrics

--- capstone/metrics/script.py
+++ capstone/metrics/script.py
@@ -1,9 +1,18 @@
 import pandas as pd
+from math import sqrt
+import numpy as np   
 
-def mrr(ratings, top_n):
+def top_n_for_user(predictions, user_id, n):
+    predictions_for_user = predictions[[user_id]]
+    predictions_for_user = predictions_for_user.sort_values(by=[user_id], ascending=False)
+    return predictions_for_user.head(n=n)
+
+def mrr(ratings, predictions):
     mrr = 0.0
+
     for user_id in users:
         user_ratings = ratings[user_id].dropna()
+        top_n = top_n_for_user(predictions, user_id, 5)
 
         rank = 1.0
 
@@ -12,7 +21,6 @@ def mrr(ratings, top_n):
 
             if recommended_item in user_ratings:
                 mrr += 1/rank
-                print 'User', user_id, 'rated item', recommended_item, 'rank', rank
             
             rank += 1.0
 
@@ -20,18 +28,68 @@ def mrr(ratings, top_n):
 
     return  mrr/number_of_users
 
-headers = pd.read_csv('cbf.csv', nrows=1)
+def rmse(ratings, predictions):
+    sum_squared_error = 0.0
+    total_ratings = 0
 
-users = headers.columns[1:] 
+    for user_id in users:
+        user_ratings = ratings[user_id].dropna()
+        top_n = top_n_for_user(predictions, user_id, 5)
 
+        recommended_relevant_items = list(set(user_ratings.index) & set(top_n.index))
+
+        filtered_top_n_df = top_n.filter(items=recommended_relevant_items, axis=0)
+        filtered_user_ratings_df = user_ratings.filter(items=recommended_relevant_items, axis=0)
+
+        filtered_top_n_values = list(map(float, filtered_top_n_df[user_id]))
+        filtered_user_ratings_values = list(map(float, filtered_user_ratings_df))
+
+        error = np.subtract(filtered_top_n_values, filtered_user_ratings_values)
+        squared_error = np.power(error, 2)
+        sum_squared_error += np.sum(squared_error)
+        total_ratings += len(user_ratings)
+
+    return sqrt(sum_squared_error / total_ratings)
+
+def precision(recommended_items, user_relevant_items):
+    recommended_relevant_items = list(set(user_relevant_items.index) & set(recommended_items.index))
+
+    return float(len(recommended_relevant_items)) / float(len(recommended_items))
+
+def mean_average_precision(ratings, predictions):
+    user_id = '64'
+
+    average_precision = 0.0
+
+    for user_id in users:
+        precision_for_user = 0.0
+        user_relevant_items = ratings[ratings[user_id] > 3.0][user_id].dropna()
+
+        top_n = top_n_for_user(predictions, user_id, 5)
+        
+        for i in range(len(top_n.index)):
+            recommended_at_k = top_n_for_user(predictions, user_id, i+1)
+            candidate_item = list(top_n.index)[i]
+            
+            if candidate_item in list(user_relevant_items.index):
+                precision_for_user += precision(recommended_at_k, user_relevant_items)
+        
+        average_precision += precision_for_user / float(len(user_relevant_items))
+
+    return average_precision / float(len(users))
+
+def coverage(ratings, predictions):
+    # TBD
+
+def intralist_category_diversity(ratings, predictions):
+    # TBD
+
+def intralist_price_diversity(ratings, predictions):
+    # TBD
+
+
+users = pd.read_csv('cbf.csv', nrows=1).columns[1:] 
 predictions = pd.read_csv('cbf.csv')
-
 ratings = pd.read_csv('ratings.csv')
 
-user_id = '64'
-
-predictions_for_user = predictions[[user_id]]
-predictions_for_user = predictions_for_user.sort_values(by=[user_id], ascending=False)
-top_n = predictions_for_user.head()
-
-print 'MRR', mrr(ratings, top_n)
+print 'MRR', mrr(ratings, predictions), 'RMSE', rmse(ratings, predictions), 'MAP', mean_average_precision(ratings, predictions)
