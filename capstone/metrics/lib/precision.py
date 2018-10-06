6adbada8896257a6273b3197d42598ffb46e4fa2
Alberto Guerra <aguerra@riotgames.com>
2018-10-05 20:59:46 -0700

    Fixing MAP

--- capstone/metrics/lib/precision.py
+++ capstone/metrics/lib/precision.py
@@ -7,14 +7,15 @@ def average_precision_for_user(top_n, user_id):
   top_n_items = list(top_n['Item'])
   user_relevant_items = list(get_relevant_items_for_user(user_id)['item'])
 
+  relevant_items_count = 0
   for i in range(len(top_n['Item'])):
-    relevant_items_count = 0
     rank = i + 1
     if top_n_items[i] in user_relevant_items:
       relevant_items_count += 1
-      average_precision += relevant_items_count/rank
+      precision = float(relevant_items_count)/float(rank)
+      average_precision += precision
 
   if relevant_items_count > 0:
-    return average_precision/relevant_items_count
+    return average_precision/float(relevant_items_count)
 
-  return 0
+  return 0.0
--- capstone/metrics/test.py
+++ capstone/metrics/test.py
@@ -8,7 +8,7 @@ from lib.availability import availability_for_user
 from lib.coverage import is_user_covered, category_coverage_for_user
 from lib.diversity import intralist_price_diversity_for_user, intralist_category_diversity_for_user
 from lib.mrr import mrr_for_user
-from lib.precision import mean_average_precision_for_user
+from lib.precision import average_precision_for_user
 from lib.rmse import rmse_for_user
 from lib.serendipity import serendipity_for_user
 from lib.ndcg import ndcg
@@ -18,11 +18,11 @@ from lib.utils import read_items_from_file, read_ratings_from_file, read_predict
 set_ratings(read_ratings_from_file())
 ITEMS = read_items_from_file()
 set_items(ITEMS)
-predictions = read_predictions_from_file('mf')
+predictions = read_predictions_from_file('user-user')
 set_predictions(predictions)
 
 users = get_users()
 generate_top_n_for_all_users(users)
 
 top_n = get_top_n('804', 5)
-print top_n
+print average_precision_for_user(top_n, '804')
